f2281f516cb4a345c6097da606357cf9865ff2ca
Lucas Maciel <lucas333maciel@gmail.com>
2025-08-14 11:32:53 -0300

    feat: fist commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export function App() {
 
   return (
     <>
-      <h3>Search Home</h3>
+      <h3>Search Home :)</h3>
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>

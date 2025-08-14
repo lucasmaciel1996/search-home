0de597505e8d7a56cf175e722d7dafff3711a523
Lucas Maciel <lucas333maciel@gmail.com>
2025-08-14 10:50:29 -0300

    feat: fist commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>Search Home</h3>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+    </>
+  )
+}
+
